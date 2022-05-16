8470bddda3661382aacc4bc44241f26ed3c3563d
莫离 <327660509@qq.com>
2022-05-16 08:36:39 +0800

    Create Nicegram.js

---
+++ jiaoben/Nicegram.js
@@ -0,0 +1,12 @@
+/***********************************
+
+[rewrite_local]
+
+# ～ Nicegram解鎖會員權限（2022-04-10）
+https?:\/\/restore-access\.indream\.app\/restoreAccess\?id=\d{5,10} url echo-response text/json echo-response https://github.com/ddgksf2013/Cuttlefish/raw/master/Crack/nicegram_body.js
+
+[mitm] 
+
+hostname=restore-access.indream.app
+
+***********************************/
