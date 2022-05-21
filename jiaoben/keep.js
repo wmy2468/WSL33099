e32cd8400f3c9b24e7586b0a4f8a91de17bfb68a
莫离 <327660509@qq.com>
2022-05-22 01:24:02 +0800

    Update keep.js

--- jiaoben/keep.js
+++ jiaoben/keep.js
@@ -7,8 +7,7 @@
 [rewrite_local]
 
 # keep解锁会员
-^https://(.+).gotokeep.com(/athena/v5/people/my|/nuocha/plans) url script-response-body https://raw.githubusercontent.com/zwf234/rules/master/js/keep.js
-
+^https://(.+).gotokeep.com(/athena/v5/people/my|/nuocha/plans) url script-response-body https://raw.githubusercontent.com/WSL33099/QuantumultX/main/jiaoben/keep.js
 [mitm] 
 
 hostname = *.gotokeep.com
