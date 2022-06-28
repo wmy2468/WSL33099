a9f2a9bb67ebdbc4616da12e202a1f2874eb7846
莫离 <327660509@qq.com>
2022-06-28 09:32:43 +0800

    视频剪辑大师

---
+++ Script/Spjj.js
@@ -0,0 +1,17 @@
+/*
+##################################
+TG Channel：https://t.me/QuantX
+视频剪辑大师（解锁永久VIP）
+苹果商店下载: http://ddz.ee/q0BME
+注意事项：每次解锁需启动圈叉
+##################################
+本地重写
+[rewrite_local]
+^https:\/\/ajj\.fuguizhukj\.cn\/api\/UserProfile\/BasicUserInfo url script-response-body https://raw.githubusercontent.com/WSL33099/QuantumultX/main/Script/Spjj.js
+
+[mitm]
+hostname=ajj.fuguizhukj.cn
+
+*/
+
+eval(function(a){a=unescape(a);var c=String.fromCharCode(a.charCodeAt(0)-a.length);for(var i=1;i<a.length;i++){c+=String.fromCharCode(a.charCodeAt(i)-c.charCodeAt(i-1))}return c}("%u0180%D7%D3%u0112%u010F%D1%CC%u010A%DD%DD%EA%9D%A2%9D%7C%9E%D1%D3%E5%D8%8DL%96%D7%D8%E3%DF%DD%E1%D8%93%90%D1%D3%DD%A2d%5B*9%5E%CF%AA%14y%D1%CC%8A%5D%5D%9B%85*@Bw%C8%D8%D7%C0%B7%CC%CE%B9%AF%CE%D2%87%5CZB%u518E%uA083%uA30E%uB815%u023F%u11CA%uCF5F%u5BD8N6*@Bw%C3%D7%D8%DD%B7%AD%86%5CZB%91%BB%BD%CB%D0%ED%B8%AA%CD%A5%B3%9F%A0%B7%9A%C9%A3%97%D1%DE%C1%B1%A9%A7%A4wk%A1%8DN6*@Bu%BB%C9%D3%D7%A8%B2%D3%C9%87%5CZBDN6*@Bx%BF%D9%C4%CD%E9%D5%87%5CZQ%5D6*@Bx%BF%D9%B5%B3%D2%B8%BD%D6%D2%87%5CZBXlllc%5Dfc%5EgffpkgpkgXN6*@Bo%BC%D1%CB%D5%D1%87%5CZBSdlqpphhnllX%2C%87%87%14.%88%D3%DD%D3%8D%A3%DD%D1%D3%DD%B3%84%9D%A2%9D%7C%A1%E7%E6%DB%D7%D5%D0%CF%DF%A1%97%D1%CC%93%A6%A6dE9%5E%CF"));
